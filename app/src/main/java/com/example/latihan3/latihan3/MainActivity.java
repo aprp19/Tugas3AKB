53046757c4db4ac123b6751360f1dd354b128eee
Angga P <kierska19@gmail.com>
2020-05-01 06:34:17 +0700

    Final

---
+++ app/src/main/java/com/example/latihan3/latihan3/MainActivity.java
@@ -0,0 +1,25 @@
+/*
+NAMA    : ANGGA PUJA RESTU PRAKASA
+NIM     : 10117130
+KELAS   : IF4
+TANGGAL PENGERJAAN : 1 - MEI - 2020
+DESKRIPSI PENGERJAAN :
+1 - MEI - 2020
+-SELESAI-
+ */
+package com.example.latihan3.latihan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.latihan3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
